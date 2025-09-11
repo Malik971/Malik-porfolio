cc423f35e6c57b1c88ff7462243a26e55fd1ffda
Malik Ibo <malik97un@gmail.com>
2025-09-12 00:55:56 +0200

    ajoute du CV avec *.pdf et correction flex & md-4 dans PrivateLayout.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import photoMalik from "../assets/photo_malik.jpg";
+import cvMalik from "../assets/CV_Malik_Ibo.pdf";
 
 function About() {
   return (
@@ -52,10 +53,14 @@ function About() {
             Vercel).
           </p>
           <div className="text-center">
-            <button className="w-40 bg-[#d4af37] text-black font-semibold py-3 rounded-full shadow hover:bg-[#e3c770] transition duration-300">
-              Me contacter
-            </button>
-          </div>
+  <a
+    href={cvMalik}
+    download="CV_Malik_Ibo.pdf"
+    className="inline-block w-40 bg-[#d4af37] text-black font-semibold py-3 rounded-full shadow hover:bg-[#e3c770] transition duration-300 text-center"
+  >
+    Télécharger CV
+  </a>
+</div>
         </div>
       </div>
     </section>
--- src/layout/PrivateLayout.tsx
+++ src/layout/PrivateLayout.tsx
@@ -4,10 +4,10 @@ import Nav from "../pages/Navigation/Nav";
 
 function PrivateLayout() {
   return (
-    <section className="min-h-screen flex">
+    <section className="min-h-screen">
       <Nav />
       <main className="wrapper pt-20 md:pt-0 pl-0 md:pl-64">
-        <header className="flex justify-between text-2xl font-bold mb-4" />
+        <header className="flex justify-between text-2xl font-bold" />
         <Outlet />
       </main>
     </section>
